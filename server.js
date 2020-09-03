7a8940eaef5b8b23b3524d93d21e01aafb9d7f55
unknown <metybuza46@gmail.com>
2020-09-02 18:35:45 -0600

    added server.js,db.json and nodemodule

---
+++ server.js
@@ -0,0 +1,20 @@
+//To interact with the front end
+const express = require("express");
+// need path for filename paths
+const path = require("path");
+// need fs to read and write to files
+const fs = require("fs");
+
+// creating an "express" server
+const app = express();
+// Sets an Initial port for listeners
+const PORT = process.env.PORT || 9001;
+
+//  Initialize notesData
+
+let notesData = [];
+
+// Set up body parsing, static, and route middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "Develop/public")));
