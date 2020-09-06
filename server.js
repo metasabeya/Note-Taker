ea097020eb2fc789f4498211bb9898fcba0291b4
unknown <metybuza46@gmail.com>
2020-09-05 19:10:17 -0600

    final code

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const fs = require("fs");
 //for the express server
 const app = express();
 //the initial port for the server
-const PORT = process.env.PORT || 7001;
+const port= process.env.PORT || 7001;
 const mainDir = path.join(__dirname, "/public");
 //seting up body parsing, static 
 
